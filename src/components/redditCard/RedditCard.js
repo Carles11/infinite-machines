6595a3b97e72f99b82980cc2281ef5579c1e5b8a
Carles <carles@crix.design>
2021-04-18 13:44:11 -0400

    filtering main fetch

--- src/components/redditCard/RedditCard.js
+++ src/components/redditCard/RedditCard.js
@@ -1,65 +1,37 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { getUrlFromGif } from '../../helpers/utils'
 
 import './redditCard.css'
 
 const RedditCard = ({ reddits }) => {
-  const [redditPics] = useState([])
-  const [redditGifs] = useState([])
-  const [picIsGif, setPicIsGif] = useState(false)
-
-  useEffect(() => {
-    reddits.map((reddit, key) =>
-      reddit.url.match(/\.(gifv)$/) != null
-        ? redditGifs.push(reddit)
-        : reddit.url.match(/\.(jpeg|jpg|png)$/) != null
-        ? redditPics.push(reddit)
-        : null,
-    )
-    console.log('PAPAPAPA', redditPics)
-    console.log('popopopopop', redditGifs)
-  })
-
-  const handlePicIsGif = isGif => {
-    setPicIsGif(isGif)
-    console.log('PIPIPIPI', picIsGif)
-  }
   return (
     <div className="reddit-card">
-      <div className="object-and-details">
-        {picIsGif
-          ? redditGifs.map((reddit, key) => (
-              <>
-                <img
-                  key={key}
-                  src={getUrlFromGif(reddit.url)}
-                  alt={reddit.title}
-                />
-                <h5>{reddit.title}</h5>
-                <details>
-                  <summary role="button" aria-label="static image"></summary>
-                  <div class="object-and-details1">
-                    {redditPics.map((reddit, key) => (
-                      <>
-                        <img
-                          key={key}
-                          src="https://res.cloudinary.com/dssldws2k/image/upload/v1592923278/JordiArjo/Backgrounds/roof-4057310_640.jpg"
-                          alt={reddit.title}
-                        />
-                        <h5>{reddit.title} </h5>
-                      </>
-                    ))}
-                  </div>
-                </details>
-              </>
-            ))
-          : redditPics.map((reddit, key) => (
-              <>
-                <img key={key} src={reddit.url} alt={reddit.title} />
-                <h5>{reddit.title}egegeg</h5>
-              </>
-            ))}
-      </div>
+      {reddits.map((reddit, key) =>
+        reddit.url.match(/\.(gifv)$/) != null ? (
+          <>
+            <img key={key} src={getUrlFromGif(reddit.url)} alt={reddit.title} />
+            <h5>{reddit.title}</h5>
+            <details>
+              <summary role="button" aria-label="static reddit"></summary>
+              <div class="object-and-details1">
+                <>
+                  <img
+                    key={key}
+                    src="https://res.cloudinary.com/dssldws2k/image/upload/v1592923278/JordiArjo/Backgrounds/roof-4057310_640.jpg"
+                    alt={reddit.title}
+                  />
+                  <h5>{reddit.title} </h5>
+                </>
+              </div>
+            </details>
+          </>
+        ) : (
+          <>
+            <img key={key} src={reddit.url} alt={reddit.title} />
+            <h5>{reddit.title}egegeg</h5>
+          </>
+        ),
+      )}
     </div>
   )
 }
