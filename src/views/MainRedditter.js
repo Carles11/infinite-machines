88cb8f79385fd072a30422609f777eb374a1da57
Carles <carles@crix.design>
2021-05-13 11:08:40 -0400

    fixed loader import

--- src/views/MainRedditter.js
+++ src/views/MainRedditter.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { fetchReddits } from '../helpers/utils.js'
 
 import RedditList from '../components/redditList/RedditList.js'
-import Loader from '../components/loader/Loader'
+import Loader from '../components/loader/Loader.js'
 
 const MainRedditter = () => {
   const [reddits, setReddits] = useState([])
